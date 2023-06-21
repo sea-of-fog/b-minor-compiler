409457cd0671e72acb1172258e38aef9289eb1c8
Krzysztof Szymański <kszymanski926@gmail.com>
2023-06-21 18:55:08 +0200

    BugFix: wrong return type in op_parser

--- parser.ml
+++ parser.ml
@@ -10,28 +10,28 @@ let rec number_parser ts =
                      | Number n -> Some (NumE n, rest)
                      | _ -> None
 
-                 and bracket_parser ts =
-                     match ts with
-                     | [] -> None
-                     | t::rest -> match t with
-                     | OpenParen -> match expr_parser rest with
-                                | None -> None
-                                  | Some exp::rest -> match rest with
-                                                      | ClosedParen::rest -> Some (exp, rest)
-                                                      | _ -> None
-                                                  and
+and bracket_parser ts =
+    match ts with
+    | [] -> None
+    | t::rest -> match t with
+                 | OpenParen -> match expr_parser rest with
+                 | None -> None
+                 | Some (exp,rest) -> match rest with
+                                      | ClosedParen::rest -> Some (exp, rest)
+                                      | _ -> None
+                                                
 
-op_parser ts =
+and op_parser ts =
         match expr_parser ts with
         | None -> None
         | Some (exp1, rest) -> match rest with
                                | (Op oper)::rest -> match expr_parser rest with
                                                   | None -> None
-                                                  | Some (exp2, rest) -> OpE oper, exp1, exp2
+                                                  | Some (exp2, rest) -> Some ((OpE oper, exp1, exp2), rest)
                                | _ -> None
 
 
-                           and expr_parser ts =
+and expr_parser ts =
     match bracket_parser ts with
     | Some (expr, rest) -> Some (expr, rest)
     | None -> match op_parser ts with
