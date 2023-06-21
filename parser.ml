bf220f2b58dfdbab7663033875e8532587f1ae18
Krzysztof Szymański <kszymanski926@gmail.com>
2023-06-21 18:52:23 +0200

    BugFix: functions in parser are mutually recursive

--- parser.ml
+++ parser.ml
@@ -3,34 +3,35 @@ type expr =
     | NumE of int
     | OpE of op * expr * expr
 
-let number_parser ts =
-    match ts with
-    | [] -> None
-    | t::rest -> match t with
-                 | Number n -> Some (NumE n, rest)
-                 | _ -> None
+let rec number_parser ts =
+        match ts with
+        | [] -> None
+        | t::rest -> match t with
+                     | Number n -> Some (NumE n, rest)
+                     | _ -> None
 
-let bracket_parser ts =
-    match ts with
-    | [] -> None
-    | t::rest -> match t with
-                 | OpenParen -> match expr_parser rest with
-                                  | None -> None
+                 and bracket_parser ts =
+                     match ts with
+                     | [] -> None
+                     | t::rest -> match t with
+                     | OpenParen -> match expr_parser rest with
+                                | None -> None
                                   | Some exp::rest -> match rest with
                                                       | ClosedParen::rest -> Some (exp, rest)
                                                       | _ -> None
+                                                  and
 
-let op_parser ts =
-    match expr_parser ts with
-    | None -> None
-    | Some (exp1, rest) -> match rest with
-                           | (Op oper)::rest -> match expr_parser rest with
-                                              | None -> None
-                                              | Some (exp2, rest) -> OpE oper, exp1, exp2
-                           | _ -> None
+op_parser ts =
+        match expr_parser ts with
+        | None -> None
+        | Some (exp1, rest) -> match rest with
+                               | (Op oper)::rest -> match expr_parser rest with
+                                                  | None -> None
+                                                  | Some (exp2, rest) -> OpE oper, exp1, exp2
+                               | _ -> None
 
 
-let rec expr_parser ts =
+                           and expr_parser ts =
     match bracket_parser ts with
     | Some (expr, rest) -> Some (expr, rest)
     | None -> match op_parser ts with
