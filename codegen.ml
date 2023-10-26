ff6a6f1a6ddefbc8a57d4067a62b5f5ebc0b952d
Krzysztof Szymański <kszymanski926@gmail.com>
2023-10-26 19:12:53 +0200

    working code generator

--- codegen.ml
+++ codegen.ml
@@ -21,3 +21,42 @@ let scratch_alloc table =
                                          | None -> None
                                          | Some x -> Some true)
                                table)
+
+let rec codegen exp table =
+    match exp with
+    | NumE(n) -> let free, new_table = scratch_alloc table
+                 in ( ("MOVQ $"^(Printf.sprintf "%d" n)^", "^free)::[],
+                     new_table,
+                     free)
+    | OpE(Add, exp1, exp2) -> let left_code, left_table, left_res = codegen exp1 table in
+                              let right_code, right_table, right_res = codegen exp2 left_table in
+                              ( ("ADDQ "^left_res^", "^right_res)::(right_code@left_code),
+                                scratch_free left_res right_table,
+                                right_res)
+    | OpE(Sub, exp1, exp2) -> let left_code, left_table, left_res = codegen exp1 table in
+                              let right_code, right_table, right_res = codegen exp2 left_table in
+                              ( ("SUBQ "^left_res^", "^right_res)::(right_code@left_code),
+                                scratch_free left_res right_table,
+                                right_res)
+    | OpE(Mul, exp1, exp2) -> let left_code, left_table, left_res = codegen exp1 table in
+                              let right_code, right_table, right_res = codegen exp2 left_table in
+                              ( ("MOVQ %rax, "^right_res) ::
+                                ("IMUL "^right_res) ::
+                                ("MOVQ "^left_res^", %rax") ::
+                                (right_code@left_code),
+                                scratch_free left_res right_table,
+                                right_res)
+
+module O = Out_channel
+
+let rec output_lines channel lines =
+    match lines with
+    | [] -> ()
+    | line::lines -> let () = O.output_string channel ("\t"^line^"\n")
+                     in output_lines channel lines
+
+
+let write_code tgt exp =
+    let code, _, _ = codegen exp scratch_table in
+    let channel = O.open_text tgt in
+    output_lines channel (List.rev code)
