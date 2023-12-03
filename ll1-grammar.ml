e16d111295e97d5971fd31eb7af2b951f314f389
Krzysztof Szymański <kszymanski926@gmail.com>
2023-12-03 22:18:47 +0100

    cleaning up

--- ll1-grammar.ml
+++
@@ -1,9 +0,0 @@
-(* type 't is for terminals, 'n for nonterminals *)
-type ('t, 'n) sentencial-form =
-    (('t, 'n) Either.t) list
-
-type ('t, 'n) rule =
-    't * ('t, 'n) sentencial-form list
-
-type ('t, 'n) ll1-grammar =
-    (('t, 'n) rule) list
