f500cf7a3deace25a16473534b3073179f05cd85
Krzysztof Szymański <kszymanski926@gmail.com>
2023-12-06 22:52:41 +0100

    added continuation data structure for codegen

--- code.ml
+++ code.ml
@@ -1,19 +1,50 @@
-type t = string list
+type t = 
+    | Nil
+    | Single of string
+    | List   of string list
+    | Prefix of string * t
+    | Suffix of t * string
+    | Concat of t * t
 
-let empty = []
+let empty = Nil 
 
 let single_line s = 
-    [s]
+    Single s 
 
 let add_line line code =
-    code@[line]
+    Suffix (code, line)
 
-let concat c1 c2 = c1@c2
+let concat c1 c2 = 
+    Concat (c1, c2)
 
-let prefix line lines =
-    line::lines
+let prefix line code =
+    Prefix (line, code)
 
 let from_list lines =
-    lines
+    List lines
 
-let to_lines code = code
+let rec append_cps xs ys k =
+    match xs with
+    | []     -> k ys
+    | x::xs  -> append_cps xs ys (fun res -> k (x::res))
+
+let rec suffix_cps xs x k =
+    match xs with
+    | []    -> x::[]
+    | r::rs -> suffix_cps rs x (fun res -> k (r::res))
+
+let rec to_lines_cps code k =
+    match code with
+    | Nil             -> k []
+    | Single l        -> k [l]
+    | List ls         -> k ls
+    | Prefix (l, ls)  -> to_lines_cps ls 
+                             (fun res -> k (l::res))
+    | Suffix (ls, l)  -> to_lines_cps ls 
+                             (fun res -> suffix_cps res l k)
+    | Concat (l1, l2) -> to_lines_cps l1 (fun l1 -> 
+                             to_lines_cps l2 (fun l2 ->
+                                 append_cps l1 l2 k))
+
+let to_lines code = 
+    to_lines_cps code (fun x -> x)
