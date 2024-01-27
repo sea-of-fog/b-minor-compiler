9dab250910144b5ae2d0011c3c19a2a0fa7f0300
Krzysztof Szymański <kszymanski926@gmail.com>
2024-01-27 12:21:52 +0100

    work on addressTable

--- addressTable.ml
+++ addressTable.ml
@@ -1,40 +1,56 @@
+open Syntax
+
 type block_state = 
-    { scope_stack    = int list;
-      temp_stack     = int list;
-      temps          = int;
-      free_temps     = int list;
-      busy_registers = register list;
-      free_register  = register list
+    { scope_stack    : int list;
+      temps          : int;
+      free_temps     : int list;
+      free_registers : register list;
+      name           : string;
     }
 
 type my_state = block_state list
+
+(* TODO: separate name and scope stack and write "methods" to access/change them *)
+(* TODO: add getters/setters *)
 
 module State = struct
     type t = my_state
 end
 
 module M = StateMonad.Make(State)
-open M
 
-type t = M.t
+type 'a t = 'a M.t
 
 let return = 
-    failwith "not implemented"
+    M.return
 
 let ( let* ) =
     M.( let* )
 
+let get =
+    M.get
+
+let set =
+    M.set
+
 let global_state = 
-    failwith "not implemented"
+    { scope_stack = [];
+      temps = 0;
+      free_temps = [];
+      free_registers = R10::R11::R12::R13::R14::R15::[];
+      name = "global"
+    }::[]
 
 let run =  
     M.run global_state
 
-let get_top =
-    failwith "not implemented"
+let get_top : block_state t =
+    let* top::rest = get in
+        return top
 
-let set_top =
-    failwith "not implemented"
+let set_top new_top =
+    let* top::rest = get in
+        set @@ new_top::rest
 
 let rec compute_offset scope_stack scope pos =
     match scope_stack with
@@ -52,33 +68,56 @@ let free = function
         return ()
     | RegisterMem r ->
         let* state = get_top in
-        let (busy, free) = (state.busy_registers, state.free_registers) in
-        let busy = List.filter (<> r) busy in
+        let free = state.free_registers in
         let free = r::free in
-        let* () = set_top {  }
+        set_top { name = state.name;
+                  scope_stack = state.scope_stack;
+                  free_temps = state.free_temps;
+                  temps = state.temps;
+                  free_registers = free}
     | TempMem ind ->
         let* state = get_top in
-        let temp_list = state.temp_list in
-        let temp_list = ind::temp_list in
-        let* () = set {  }
+        let temps = state.free_temps in
+        let temps = ind::temps in
+        set_top { name = state.name;
+                  scope_stack = state.scope_stack;
+                  free_temps = temps;
+                  temps = state.temps;
+                  free_registers = state.free_registers}
 
 let alloc = function
     | GlobalLoc s ->
         return @@ GlobalMem s
-    | LocalLoc { scope, pos } ->
+    | LocalLoc { scope; pos } ->
+        let* state = get_top in
+           return @@ LocalMem (compute_offset state.scope_stack scope pos) 
     | TempLoc ->
         let* state = get_top in
         begin match state.free_registers with
         | r::rs ->
-            let* () = set_top {} in
-                return r
+                let* () = set_top { name = state.name;
+                                    free_registers = rs; 
+                                    temps = state.temps; 
+                                    free_temps = state.free_temps; 
+                                    scope_stack = state.scope_stack} 
+                in return r
         | [] -> begin match state.free_temps with
-                      | t::ts -> let* () = set_top {} in
-                                    return t
+                      | t::ts -> let* () = set_top { name = state.name;
+                                                     free_registers = state.free_registers;
+                                                     temps = state.temps;
+                                                     free_temps = ts;
+                                                     scope_stack = state.scope_stack} 
+                                 in return @@ TempMem 
                       | []    -> let temps = state.temps in
-                                 let temps = temps+1
-                                    let* () = set_top { temps } in
-                                        return @@ temps 
+                                 let temps = temps+1 in
+                                    let* () = set_top { temps = temps;
+                                                        name = state.name;
+                                                        free_registers = state.free_registers;
+                                                        temps = state.temps;
+                                                        free_temps = ts;
+                                                        scope_stack = state.scope_stack}
+                                    in return @@ TempMem temps 
+                end
         end
 
 let state_of_block_data =
@@ -87,9 +126,12 @@ let state_of_block_data =
 let block_data_of_state =
     failwith "not implemented"
 
-let open_scope block_data = 
+let add_top s =
     let* state = get in
-        set @@ (state_of_block_data block_data)
+        set @@ s::state
+
+let open_scope block_data = 
+    add_top (state_of_block_data block_data)
 
 let close_scope =
     let* top::rest = get in
